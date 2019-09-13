563191e6b83f9390bbddd36c8d8257238489334a
Ken Domke <kendomke@users.noreply.github.com>
2019-09-12 17:18:39 -0700

    Coin pickups start with random rotation

--- Assets/Scripts/CoinsPickup.cs
+++ Assets/Scripts/CoinsPickup.cs
@@ -6,6 +6,11 @@ using SBR;
 public class CoinsPickup : Pickup {
     public int singleCoinValue = 10;
 
+    private void Start()
+    {
+       transform.Rotate(new Vector3(0, 0, Random.Range(0, 360)));
+    }
+
     protected override void TakeEffect(PlayerShip ship) {
         ship.AddCoins(singleCoinValue);
         MessageManager.inst.ShowMessage("Acquired <color=green>Coin</color>", 3.0f);
